a98aab81fbec76dc1575bad637a100a0fafc0344
KurokuLarbouno <k850701.lee@gmail.com>
2019-12-26 01:09:49 +0800

    walk

--- Assets/DOG.cs
+++ Assets/DOG.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class DOG : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject tracker;
+    public Vector3 post_pose;
+    Animation anim;
     void Start()
     {
-        
+        GameObject tempObj = transform.GetChild(0).gameObject;
+        anim = tempObj.GetComponent<Animation>();
+        anim.Play();
+        Debug.Log(anim);
     }
 
     // Update is called once per frame
     void Update()
     {
-        tracker = GameObject.Find("traker");
-        this.transform.position = tracker.transform.position;
+        float temp, length;
+        temp = Mathf.Pow(transform.position.x - post_pose.x, 2) + Mathf.Pow(transform.position.z - post_pose.z, 2);
+        length = Mathf.Sqrt(temp);
+        if(length >= 0.1 && !anim.isPlaying) {
+            anim.Play("Default Take");
+        }else if(length < 0.1)
+        {
+            //anim.Stop();
+        }
+        post_pose = transform.position;
     }
 }
