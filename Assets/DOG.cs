6b01fe929bc829c138c1f90dfe2a40e983ad7a64
kurukolee <k850701.lee@gmail.com>
2019-12-25 20:45:23 +0800

    位移

---
+++ Assets/DOG.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOG : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject tracker;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tracker = GameObject.Find("traker");
+        this.transform.position = tracker.transform.position;
+    }
+}
